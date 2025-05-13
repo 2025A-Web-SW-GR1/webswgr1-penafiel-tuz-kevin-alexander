4b008245f286ba2643abc31c823a234e71f81da8
KealPetu <kpenafiel99@gmail.com>
2025-05-13 17:34:59 -0500

    tipos de variables en typescrtipt

--- Trabajos/13-05-2025/assets/js/02-typescript.ts
+++ Trabajos/13-05-2025/assets/js/02-typescript.ts
@@ -1,1 +1,33 @@
-console.log('Hola TypeScript');
+console.log("Hola Typescript");
+// var nombre = "algo"; // NO VAMOS A USAR VAR NUNCA
+let nombres = "Adrian";
+nombres = "A";
+nombres = 'C';
+// nombres = 1;
+let nombreTS: string = "";
+console.log(typeof nombres, "nombres");
+let numeros = 1;
+let numerosTS: number = 1;
+console.log(typeof numeros, "numeros");
+numeros = 1.1; // Decimales
+console.log(typeof numeros, "numeros decimales");
+let booleanos = true;
+let booleanosTS: boolean = false;
+booleanos = false;
+console.log(typeof booleanos, "booleanos");
+let nulos = null;
+let nulosTS: null = null;
+console.log(typeof nulos, "nulos");
+
+let arreglos = [];
+let arreglosTS: number[] = [];
+let arreglosTS2: Array<number> = [];
+console.log(typeof arreglos, "arreglos");
+
+let objetos = {};
+let objetosTS: object = {};
+console.log(typeof objetos, "objetos");
+
+let undefineds = undefined;
+let undefinedsTS: undefined = undefined;
+console.log(typeof undefineds, "undefineds");

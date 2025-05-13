dd5729d1ecdc5eebf8a1b324ff087228913d7312
KealPetu <kpenafiel99@gmail.com>
2025-05-13 17:17:18 -0500

    css y typescript

---
+++ Trabajos/13-05-2025/assets/js/01-javascript.js
@@ -0,0 +1,16 @@
+console.log('World')
+
+//Tipos de variables
+//Mutables
+
+let nombre = "Kevin";
+nombre = "Alexander";
+// nombre = 18;
+// nombre = function(){
+//     return 1;
+// }
+
+const cedula = "1755968169";
+// cedula = 123; 
+//  Uncaught TypeError: invalid assignment to const 'cedula'
+//  <anonymous> http://127.0.0.1:8080/assets/js/01-javascript.js:14
